bc58b7a187c850406dfa61bd1ac4de3f0cabc50b
Victor Lee <victorchristopherlee@gmail.com>
2025-05-21 22:04:24 -0500

    refactor 1

--- nested_if.py
+++ nested_if.py
@@ -2,17 +2,15 @@ sport = "basketball"
 p1_score = 10
 p2_score = 9
 
-if sport.lower() == "golf":
-    if p1_score == p2_score:
-        print("It's a draw!")
-    elif p1_score < p2_score:
+if p1_score == p2_score:
+    print("It's a draw!")
+elif sport.lower() == "golf":
+    if p1_score < p2_score:
         print("P1 wins")
     else:
         print("P2 wins")
 elif sport.lower() == "basketball":
-    if p1_score == p2_score:
-        print("It's a draw!")
-    elif p1_score > p2_score:
+    if p1_score > p2_score:
         print("P1 wins")
     else:
         print("P2 wins")
