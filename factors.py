b8ab24b98e5a879a2997decf491e81941760cd43
Victor Lee <victorchristopherlee@gmail.com>
2025-05-27 19:03:13 -0500

    refactor

--- factors.py
+++ factors.py
@@ -1,8 +1,29 @@
-input = int(input("Enter a number: "))
-print(input)
-number = 1
+# input = int(input("Enter a number: "))
+# print(input)
+# number = 1
 
-while number < input:
-    if input % number == 0:
-        print(f"{number} is a factor of {input}")
-    number += 1
+# while number < input:
+#     if input % number == 0:
+#         print(f"{number} is a factor of {input}")
+#     number += 1
+
+
+def find_factors(number):
+    """Find all factors of a given number."""
+    factors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            factors.append(i)
+    return factors
+
+def main():
+    try:
+        num = int(input("Enter a number: "))
+        factors = find_factors(num)
+        for factor in factors:
+            print(f"{factor} is a factor of {num}")
+    except ValueError:
+        print("Please enter a valid integer.")
+
+if __name__ == "__main__":
+    main()
