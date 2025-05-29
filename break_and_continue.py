c1a6b274067dca1b34deed085318f482201a3fe0
Victor Lee <victorchristopherlee@gmail.com>
2025-05-29 15:30:40 -0500

    Implements a basic while loop with a break
    Demonstrates the use of a `while` loop along with a `break` statement to exit the loop when a condition is met.

---
+++ break_and_continue.py
@@ -0,0 +1,12 @@
+# for n in range(4):
+#     if n == 2:
+#         print("there goes 2")
+#         continue
+#     print(n)
+
+n = 0
+while True:
+    print(n)
+    if n > 5:
+      break
+    n += 1  
