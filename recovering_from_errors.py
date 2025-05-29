66f6455c7827d83df5ef73dd22d857b11a4bc433
Victor Lee <victorchristopherlee@gmail.com>
2025-05-29 16:09:01 -0500

    Adds NameError example to error recovery
    Expands the examples of common Python errors to include a NameError.
    
    Illustrates how a NameError occurs when trying to use an undefined variable.

--- recovering_from_errors.py
+++ recovering_from_errors.py
@@ -7,10 +7,19 @@
 # ValueError: invalid literal for int() with base 10: 'Hello'
 
 # TypeError: This code will raise a TypeError because you cannot concatenate a string and an integer.
-"1" + 2
+# "1" + 2
 #  Result:
 # Traceback (most recent call last):
 #   File "/Users/victorclee/Documents/learning/Python/recovering_from_errors.py", line 9, in <module>
 #     "1" + 2
 #     ~~~~^~~
 # TypeError: can only concatenate str (not "int") to str
+
+# NameError: This code will raise a NameError because the variable `does_not_exist` has not been defined.
+print(does_not_exist)
+# Result:
+# Traceback (most recent call last):
+#   File "/Users/victorclee/Documents/learning/Python/recovering_from_errors.py", line 18, in <module>
+#     print(does_not_exist)
+#           ^^^^^^^^^^^^^^
+# NameError: name 'does_not_exist' is not defined
