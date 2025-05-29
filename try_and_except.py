4c72da95934ff7f475a004dd5315858c156dd01c
Victor Lee <victorchristopherlee@gmail.com>
2025-05-29 16:37:13 -0500

    Bare except
    Bare except is not recommended as it catches all exceptions and makes debugging difficult.

--- try_and_except.py
+++ try_and_except.py
@@ -5,12 +5,19 @@
 # except ValueError:
 #     print("Please enter a valid integer.")
 
-try:
-    1/0
-    print("This will not print because of the error above.")
-except TypeError:
-    print("Error: Both inputs must be numbers.")
-except ZeroDivisionError:
-    print("Error: Cannot divide by zero.")
+# try:
+#     1/0
+#     print("This will not print because of the error above.")
+# except TypeError:
+#     print("Error: Both inputs must be numbers.")
+# except ZeroDivisionError:
+#     print("Error: Cannot divide by zero.")
 
-print("Done with try and except.")
+# print("Done with try and except.")
+
+
+try:
+    # Some code here
+except:
+    print("An error occurred.")
+# Bare except is not recommended as it catches all exceptions and makes debugging difficult.
