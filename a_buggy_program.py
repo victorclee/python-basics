110c4760d4672b6ac480a4b68d203981f4a2e4af
Victor Lee <victorchristopherlee@gmail.com>
2025-06-03 15:47:15 -0500

    Adds debug print statement
    Adds a debug print statement to trace the value of variables during execution.
    This helps in identifying the state of the `new_word` string during the loop.

--- a_buggy_program.py
+++ a_buggy_program.py
@@ -2,6 +2,7 @@ def add_underscores(word):
     new_word = "_"
     for char in word:
         new_word = new_word + char + "_"
+        print(f"Current character: {char}, New word: {new_word}")
     return new_word
 
 
