57a14f6efaae65d454aff771f56c388288d10071
Victor Lee <victorchristopherlee@gmail.com>
2025-06-12 18:45:08 -0500

    Demonstrates list manipulation in Python
    Illustrates various list methods such as insert and append to modify list contents.
    
    Removes the old numbers example and adds a colors example.

--- lists.py
+++ lists.py
@@ -1,9 +1,35 @@
-numbers = [1, 2, 3]
+# numbers = [1, 2, 3]
 
-numbers[1:] = [200, 300, 400]
+# numbers[1:] = [200, 300, 400]
 
-print(numbers)
+# print(numbers)
 
-numbers[1:] = [0]
+# numbers[1:] = [0]
 
-print(numbers)
+# print(numbers)
+
+# numbers.insert(1, "two")
+
+# print(numbers)
+
+# numbers.append(9)
+
+# print(numbers)
+
+# numbers.extend((10, 11, 12))
+
+# print(numbers)
+
+# numbers.pop(2)
+
+# print(numbers)
+
+colors = ["r", "g", "b"]
+
+colors.insert(1, "o")
+
+print(colors)
+
+colors.append("v")
+
+print(colors)
