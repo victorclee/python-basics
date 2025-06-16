f7a98a40cd5475388cc7fe21b1a3c07c9eef6502
Victor Lee <victorchristopherlee@gmail.com>
2025-06-16 16:53:26 -0500

    Demonstrates list sorting in Python
    Illustrates in-place sorting of lists using the `sort()` method.
    Shows examples with both strings and numbers, including reverse sorting.

---
+++ lists_sort.py
@@ -0,0 +1,17 @@
+colors = ["red", "yellow", "green", "blue"]
+
+colors.sort()  # Sorts the list in place
+
+print(colors)  # Output: ['blue', 'green', 'red', 'yellow']
+
+numbers = [1, 10 , 5, 3]
+
+numbers.sort()  # Sorts the list in place 
+
+print(numbers)  # Output: [1, 3, 5, 10]
+
+colors.sort(reverse=True)  # Sorts the list in place in reverse order
+numbers.sort(reverse=True)  # Sorts the list in place in reverse order
+print(colors)  # Output: ['yellow', 'red', 'green', 'blue']
+print(numbers)  # Output: [10, 5, 3, 1] 
+
