3e3c2e7bb73b70052edba9976f29f286714fa00a
Victor Lee <victorchristopherlee@gmail.com>
2025-06-16 16:32:47 -0500

    Demonstrates list reference vs. shallow copy
    Illustrates the difference between direct list assignment
    (creating a second reference) and creating a shallow copy.
    
    Highlights how modifications to a nested element in a
    shallow copy affect the original list.

--- create_a_second_reference_to_a_list.py
+++ create_a_second_reference_to_a_list.py
@@ -1,14 +1,35 @@
+# matrix = [[1,2], [3,4]]
+
+# print(matrix)
+
+# the_matrix = matrix
+
+# print(the_matrix)
+
+# the_matrix[1] = ["Neo", "Trinity"]
+
+# print(matrix)
+
+# print(id(matrix))
+# print(id(the_matrix))
+
 matrix = [[1,2], [3,4]]
 
-print(matrix)
-
-the_matrix = matrix
-
-print(the_matrix)
-
-the_matrix[1] = ["Neo", "Trinity"]
-
-print(matrix)
+the_matrix = matrix[:] # Creating a shallow copy
 
 print(id(matrix))
 print(id(the_matrix))
+
+the_matrix[1] = ["Neo", "Trinity"]
+
+print(matrix)  # Original matrix remains unchanged
+print(the_matrix)  # Modified matrix copy
+
+the_matrix[0][0] = "Morpheus"  # Modifying an element in the first row
+print(the_matrix)
+print(matrix)  # Original matrix is affected because it's a shallow copy
+
+print(id(matrix[0]))
+print(id(the_matrix[0]))
+
+# Shallow copies contain copied references. They don't copy the objects themselves.
