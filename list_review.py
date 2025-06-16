cd3768dd9ed95e9e91ff0f52d8661df99067fc79
Victor Lee <victorchristopherlee@gmail.com>
2025-06-16 16:19:32 -0500

    Adds string split and list comprehension examples
    Demonstrates string splitting and list comprehension
    for calculating string lengths within a list.

--- list_review.py
+++ list_review.py
@@ -10,5 +10,11 @@ print(food[0:2])
 
 print(food[-1])
 
+foods = "eggs, fruit, orange juice"
+breakfast = foods.split(", ")
 
+print(len(breakfast))
+print(breakfast)
 
+lengths = [len(item) for item in breakfast]
+print(lengths)
