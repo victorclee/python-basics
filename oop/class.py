8299c510c55424ab06e7ec5a292a89eaa5e0b60d
Victor Lee <victorchristopherlee@gmail.com>
2025-06-17 16:39:06 -0500

    Adds dunder methods to classes
    Adds `__add__` and `__str__` methods to the Point class for
    vector addition and string representation.
    
    Adds `description`, `speak`, and `__str__` methods to the Doggo
    class for providing information about the dog and making it speak.
    
    Also includes docstrings.

--- oop/class.py
+++ oop/class.py
@@ -1,12 +1,24 @@
 # constructor and attributes
 class Point:
+    ''' A class to represent a point in 2D space. '''
     dimensions = 2  # class attribute
     def __init__(self, x, y): # constructor
         self.x = x
         self.y = y
-
+    def __add__(self, other):
+        self.x = self.x + other.x
+        self.y = self.y + other.y
+    def __str__(self):
+        return f"Point at x:{self.x}, y:{self.y}"
 class Doggo:
+    ''' A class to represent a dog. '''
     species = "Canis familiaris"  # class attribute
     def __init__(self, name, age):
         self.name = name # instance attribute
         self.age = age # instance attribute
+    def description(self):
+        return f"{self.name} is {self.age} years old, and is a good doggo."
+    def speak(self, sound):
+        return f"{self.name} says {sound}!"
+    def __str__(self):
+        return f"{self.name} is {self.age} years old."
